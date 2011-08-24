236aadd2fce6da8a44c09c1a370c5c87057a2d9b
Jose Naves <josenaves@gmail.com>
2011-08-24 00:11:38 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenidPoc::Application.config.secret_token = 'cc7e4a969005ce63485b03e8dcb12a69becf9a5a80a482a89b49cef2e93684f82a2fdabbf9a9b97512375c2b26d91cdb2966421b7f9f0914e41e33cf430ec4b3'
